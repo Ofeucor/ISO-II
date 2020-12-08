71a2635690fe260185b4942a68717d70d3afad30
sergiomartindg <sergiomartindg@gmail.com>
2020-12-08 12:25:45 +0100

    Add constructor Bebida

--- src/main/java/es/uclm/esi/isoft2/b03/restaurante/Dominio/Bebida.java
+++ src/main/java/es/uclm/esi/isoft2/b03/restaurante/Dominio/Bebida.java
@@ -3,6 +3,11 @@ package es.uclm.esi.isoft2.b03.restaurante.Dominio;
 
 public class Bebida extends Alimento {
 
+	public Bebida(String nombre, double precio) {
+		super(nombre, precio);
+		// TODO Auto-generated constructor stub
+	}
+
 	private boolean alcohol;
 
 }
