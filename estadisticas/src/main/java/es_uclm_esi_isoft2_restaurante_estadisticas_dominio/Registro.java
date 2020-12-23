65572259fd0f78673ef74caed5f427f66ef919a7
sergiomartindg <sergiomartindg@gmail.com>
2020-12-23 13:33:00 +0100

    It5: v1.5.1 Añadido el código generado por Visual correspondiente al módulo estadísticas

---
+++ estadisticas/src/main/java/es_uclm_esi_isoft2_restaurante_estadisticas_dominio/Registro.java
@@ -0,0 +1,21 @@
+package es_uclm_esi_isoft2_restaurante_estadisticas_dominio;
+
+public class Registro {
+
+	private int idMesa;
+	private int idCamarero;
+	/**
+	 * Este string es de tipo time()
+	 */
+	private String horaInicio;
+	/**
+	 * Este string es de tipo time()
+	 */
+	private String horaFin;
+	private String estado;
+	/**
+	 * Este string es de tipo Date()
+	 */
+	private String fecha;
+
+}
